0cff73e047226c09142ffedf461e6d768a63837b
qiongtubao <810776886@qq.com>
2024-06-22 14:52:58 +0800

    增加slice类（类似于string）字符串管理类的注解

--- include/leveldb/slice.h
+++ include/leveldb/slice.h
@@ -27,44 +27,56 @@ namespace leveldb {
 class LEVELDB_EXPORT Slice {
  public:
   // Create an empty slice.
+  //空字符串
   Slice() : data_(""), size_(0) {}
 
   // Create a slice that refers to d[0,n-1].
+  //指定传参地址和长度
   Slice(const char* d, size_t n) : data_(d), size_(n) {}
 
   // Create a slice that refers to the contents of "s"
+  //string转换成Slice
   Slice(const std::string& s) : data_(s.data()), size_(s.size()) {}
 
   // Create a slice that refers to s[0,strlen(s)-1]
+  // 自己计算字符串长度
   Slice(const char* s) : data_(s), size_(strlen(s)) {}
 
   // Intentionally copyable.
+  //拷贝
   Slice(const Slice&) = default;
+  
   Slice& operator=(const Slice&) = default;
 
   // Return a pointer to the beginning of the referenced data
+  //获得数据
   const char* data() const { return data_; }
 
   // Return the length (in bytes) of the referenced data
+  //获得长度
   size_t size() const { return size_; }
 
   // Return true iff the length of the referenced data is zero
+  //判断是否为null
   bool empty() const { return size_ == 0; }
 
   // Return the ith byte in the referenced data.
   // REQUIRES: n < size()
+  //返回引用数据中的第n个字节。
   char operator[](size_t n) const {
     assert(n < size());
     return data_[n];
   }
 
   // Change this slice to refer to an empty array
+  //清空
   void clear() {
     data_ = "";
     size_ = 0;
   }
 
   // Drop the first "n" bytes from this slice.
+  //调整向后移动n位
   void remove_prefix(size_t n) {
     assert(n <= size());
     data_ += n;
@@ -72,21 +84,26 @@ class LEVELDB_EXPORT Slice {
   }
 
   // Return a string that contains the copy of the referenced data.
+  //转换成string
   std::string ToString() const { return std::string(data_, size_); }
 
   // Three-way comparison.  Returns value:
   //   <  0 iff "*this" <  "b",
   //   == 0 iff "*this" == "b",
   //   >  0 iff "*this" >  "b"
+  //比较
   int compare(const Slice& b) const;
 
   // Return true iff "x" is a prefix of "*this"
+  //判断前缀是否为x 
   bool starts_with(const Slice& x) const {
     return ((size_ >= x.size_) && (memcmp(data_, x.data_, x.size_) == 0));
   }
 
  private:
+  //存储数据的地方
   const char* data_;
+  //长度
   size_t size_;
 };
 
